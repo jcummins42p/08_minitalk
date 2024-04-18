1c54a46248be31e4b20b966d30eb4addbea27f42
jcummins42 <jcummins42@42Ubuntu.myguest.virtualbox.org>
2024-04-18 15:19:08 +0200

    Server decode function now handles extended ascii rather than only 7 bit, Plus conforms to norminette

--- src/server/server.c
+++ src/server/server.c
@@ -6,13 +6,13 @@
 /*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 17:24:19 by jcummins          #+#    #+#             */
-/*   Updated: 2024/04/18 15:16:30 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/04/18 15:18:41 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int		decode_char(const int *octet)
+int	decode_char(const int *octet)
 {
 	int				i;
 	unsigned char	c;
