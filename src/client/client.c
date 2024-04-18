4b866edeb8dfa6cb7cb45f987fbed9fb5917180f
jcummins42 <jcummins42@42Ubuntu.myguest.virtualbox.org>
2024-04-18 14:12:44 +0200

    Changed client to send bits directly rather than store them as a string first. There didn't seem to be a good reason to store the string and send it separately, and it added unnecessary time to execute

--- src/client/client.c
+++ src/client/client.c
@@ -6,13 +6,13 @@
 /*   By: jcummins <jcummins@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 17:25:14 by jcummins          #+#    #+#             */
-/*   Updated: 2024/04/17 19:26:08 by jcummins         ###   ########.fr       */
+/*   Updated: 2024/04/18 13:31:11 by jcummins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	send_null(int server_pid)
+void	send_null(const int server_pid)
 {
 	size_t	i;
 
@@ -24,90 +24,53 @@ void	send_null(int server_pid)
 	}
 }
 
-void	send_string(int server_pid, char **str)
+void	send_char(const int server_pid, const char c)
 {
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (str[i])
-	{
-		j = 0;
-		while (str[i][j])
-		{
-			if (str[i][j] == '0')
-				kill(server_pid, SIGUSR1);
-			else if (str[i][j] == '1')
-				kill(server_pid, SIGUSR2);
-			else
-			{
-				ft_printf("Error, non binary input\n");
-				break ;
-			}
-			usleep(100);
-			j++;
-		}
-		i++;
-	}
-	send_null(server_pid);
-}
-
-char	*encode_char(char c)
-{
-	char	*out;
+	char	octet[8];
 	int		val;
-	size_t	i;
+	int		i;
 
-	out = malloc(sizeof (char *) * 9);
-	if (!out)
-		return (NULL);
 	val = (int)c;
-	i = 0;
-	while (i < 8)
+	i = -1;
+	while (i++ < 7)
 	{
 		if (val % 2)
-			out[7 - i++] = '1';
+			octet[7 - i] = '1';
 		else
-			out[7 - i++] = '0';
+			octet[7 - i] = '0';
 		val >>= 1;
 	}
-	out[8] = '\0';
-	return (out);
+	while (i-- > 0)
+	{
+		if (octet[7 - i] == '0')
+			kill(server_pid, SIGUSR1);
+		else if (octet[7 - i] == '1')
+			kill(server_pid, SIGUSR2);
+		usleep(100);
+	}
 }
 
-char	**encode_string(char *str)
+void	send_string(const int server_pid, const char *str)
 {
-	char	**bin;
 	size_t	len;
 	size_t	i;
 
 	len = ft_strlen(str);
-	bin = malloc((len + 1) * sizeof(char *));
-	if (!bin)
-		return (NULL);
 	i = 0;
 	while (i < len)
-	{
-		bin[i] = encode_char(str[i]);
-		i++;
-	}
-	bin[len] = NULL;
-	return (bin);
+		send_char(server_pid, str[i++]);
+	send_null(server_pid);
 }
 
 int	main(int argc, char *argv[])
 {
-	char	**bin;
-
 	if (argc < 3)
 	{
 		ft_printf("Please provide a valid server pid and string to send\n");
 	}
 	else if (argc == 3)
 	{
-		bin = encode_string(argv[2]);
-		send_string(ft_atoi(argv[1]), bin);
+		send_string(ft_atoi(argv[1]), argv[2]);
 	}
 	return (0);
 }
